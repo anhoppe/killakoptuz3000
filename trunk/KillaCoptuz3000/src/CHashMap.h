3c2027f44f2b997c476144f43c6ef48f3cb29ec9
fabianheinemann <fabianheinemann@9386d06f-8230-0410-af72-8d16ca8b68df>
2007-06-12 23:03:35 +0000

    fixed hash map... basics seem to work now. Method for iteration is still missing.

--- trunk/KillaCoptuz3000/src/CHashMap.h
+++ trunk/KillaCoptuz3000/src/CHashMap.h
@@ -11,6 +11,7 @@
 #define HASHMAP_H
 
 #include <vector>
+#include <list>
 
 template <class T_value>
 struct KeyValuePair
@@ -27,7 +28,7 @@ public:
    ~CHashMap();
 
    /** 
-   *  retrieve element with overloaded operator []
+   *  retrieve value with overloaded operator []
    *  @param t_key key
    */
    T_value operator[](unsigned int t_key);
@@ -47,13 +48,19 @@ public:
 
 private:
    /** Hash array */
-   std::vector<std::vector<KeyValuePair<T_value>*>> m_map;
+   std::vector<std::list<KeyValuePair<T_value>*>> m_map;
 
    /** Size of primary vector */
    unsigned int m_size;
 
    /** Hash function */
    unsigned int getIndex(unsigned int t_key);
+
+   /** 
+   *  Get KeyValuePair* 
+   *  @param t_key
+   */
+   KeyValuePair<T_value>* getKeyValuePair(unsigned int t_key);
 };
 
 template <class T_value>
@@ -62,6 +69,9 @@ CHashMap<T_value>::CHashMap()
    // Size of primary vector
    // Should be a prime number
    m_size = 101;
+
+   // Initialize vector
+   m_map.resize(m_size);
 }
 
 template <class T_value>
@@ -75,12 +85,13 @@ T_value CHashMap<T_value>::operator [](unsigned int t_key)
 {
    T_value r_ret = 0;
    unsigned int a_index = getIndex(t_key);
+   std::list<KeyValuePair<T_value>*>::iterator a_it;
 
-   for (int i=0; i < m_map[a_index].size(); i++)
+   for (a_it = m_map[a_index].begin(); a_it != m_map[a_index].end(); a_it++)
    {
-      if (t_key == m_map[a_index][i]->m_key)
+      if ((*a_it)->m_key == t_key)
       {
-         r_ret = m_map[a_index][i]->m_value;
+         r_ret = (*a_it)->m_value;
          break;
       }
    }
@@ -92,14 +103,17 @@ T_value CHashMap<T_value>::operator [](unsigned int t_key)
 template <class T_value>
 void CHashMap<T_value>::add(unsigned int t_key, T_value t_value)
 {   
-   KeyValuePair<T_value>*           a_keyValue;
+   KeyValuePair<T_value>*           a_keyValue = 0;
 
-   a_keyValue = new KeyValuePair<T_value>;
+   a_keyValue = getKeyValuePair(t_key);
+   if (0 == a_keyValue)
+   {
+      a_keyValue = new KeyValuePair<T_value>;
+   }   
 
    a_keyValue->m_key       = t_key;
    a_keyValue->m_value     = t_value;
-
-   unsigned int a = getIndex(t_key);
+   
    m_map[getIndex(t_key)].push_back(a_keyValue);
 }
 
@@ -108,4 +122,40 @@ unsigned int CHashMap<T_value>::getIndex(unsigned int t_key)
 {
    return t_key % m_size;
 }
+
+template <class T_value>
+KeyValuePair<T_value>* CHashMap<T_value>::getKeyValuePair(unsigned int t_key)
+{
+   KeyValuePair<T_value>* r_ret = 0;
+   unsigned int a_index = getIndex(t_key);
+
+   std::list<KeyValuePair<T_value>*>::iterator a_it;
+
+   for (a_it = m_map[a_index].begin(); a_it != m_map[a_index].end(); a_it++)
+   {
+      if ((*a_it)->m_key == t_key)
+      {
+         r_ret = *a_it;
+         break;
+      }
+   }
+
+   return r_ret;
+}
+
+template <class T_value>
+void CHashMap<T_value>::remove(unsigned int t_key)
+{   
+   KeyValuePair<T_value>* a_keyValuePair = 0;
+   unsigned int a_index = getIndex(t_key);
+
+   a_keyValuePair = getKeyValuePair(t_key);
+
+   if (0 != a_keyValuePair)
+   {
+      m_map[a_index].remove(a_keyValuePair);
+      delete a_keyValuePair;
+   }
+}
+
 #endif
