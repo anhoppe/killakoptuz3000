1d6b9f9c23decc670fb637f4bf97f0d7c0377b13
anhoppe <anhoppe@9386d06f-8230-0410-af72-8d16ca8b68df>
2007-06-12 18:09:09 +0000

    added CEvent module

---
+++ KillaCoptuz3000/src/CEvent.h
@@ -0,0 +1,31 @@
+// ***************************************************************
+//  CEvent   version:  1.0   ·  date: 06/11/2007
+//  -------------------------------------------------------------
+//  
+//  -------------------------------------------------------------
+//  Copyright (C) 2007 - All Rights Reserved
+// ***************************************************************
+// 
+// ***************************************************************
+
+#ifndef CEVENT_H
+#define CEVENT_H
+
+#include <list>
+
+enum EEventType {e_dying, e_delete, e_collided};
+
+class CEvent
+{
+public:
+   CEvent();
+   ~CEvent();
+
+  /** List of participating object id's*/
+  std::list<unsigned int> m_objectList;
+
+  /** Event type*/
+  EEventType m_event;  
+};
+
+#endif
